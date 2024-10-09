7182a0540d0f16bdcc2f86e90665e8fa7b1e7364
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-10-08 23:04:27 -0300

    Aplicado Encapsulamento

--- src/scripts/funcionario.js
+++ src/scripts/funcionario.js
@@ -20,7 +20,28 @@ function Pessoa(nome) {
 // Função Construtora;
 function Funcionario(nome, cargo, salario) {
     this.cargo = cargo;
-    this.salario = salario;
+    // this.salario = salario;
+    // O Encapsulamento envolve tornar variáveis privadas, impedindo sua modificação ou acesso se não necessário;
+    // Para tornar uma variável privada, não podemos usar o "this", precisamos
+    // declarar a variável, e por convenção, é utilizado o "_" antes do nome da mesma
+    // para identifica-la como privada;
+    let _salario = salario;
+    // Para acessarmos o valor de uma variável privada precisamos criar um Método;
+    // Chamamos, por convenção, funções que Retornam um valor privado como "get" (getters e setters);
+    this.getSalario = function() {
+        return _salario;
+    }
+
+    // Para alterarmos o valor de uma variável privada também precisamos criar um Método;
+    // Chamamos, por convenção, funções que Atribuem um valor privado como "set";
+    this.setSalario = function(valor) {
+        // Aqui podemos realizar validações se necessário;
+        // Verificação para ver se é String ao invés de ser um Number; 
+        if (typeof valor === 'number') {
+            _salario = valor;
+        }
+    }
+
     // Para fazer uma Herança no JS é necessário chamar a Função 
     // Construtora logo depois dos atributos da classe que irá Herda-lá;
 
@@ -34,3 +55,10 @@ divisoria("Saudação do Funcionário");
 funcionario1.dizOi();
 divisoria("Cargo do Funcionário");
 funcionario1.dizCargo();
+divisoria("Salário do Funcionário");
+console.log(funcionario1.getSalario());
+divisoria("Novo Salário do Funcionário");
+
+funcionario1.setSalario(7000);
+funcionario1.setSalario('mil'); // Tentando mudar o valor para String para testar a validação
+console.log(funcionario1.getSalario());
